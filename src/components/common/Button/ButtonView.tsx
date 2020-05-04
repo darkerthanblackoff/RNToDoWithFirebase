826f5a5de41bc7dd1ed1fcc901c10324a310236e
Mihail Lemeza <darkerthanblackoff@gmail.com>
2020-05-05 02:02:08 +0300

    Add style prop

--- src/components/common/Button/ButtonView.tsx
+++ src/components/common/Button/ButtonView.tsx
@@ -5,6 +5,7 @@ import {
   Platform,
   Text,
   View,
+  ViewStyle,
 } from 'react-native';
 
 import { styles } from './styles';
@@ -12,11 +13,17 @@ import { styles } from './styles';
 interface ButtonProps {
   label?: string;
   color?: string;
+  style?: ViewStyle;
   onPress?: () => void;
 }
 
 const ButtonView = (props: ButtonProps) => {
-  const { label = 'Label', color = '#F96060', onPress = () => {} } = props;
+  const {
+    label = 'Label',
+    color = '#F96060',
+    onPress = () => {},
+    style,
+  } = props;
 
   switch (Platform.OS) {
     case 'android':
@@ -25,7 +32,7 @@ const ButtonView = (props: ButtonProps) => {
           onPress={onPress}
           background={TouchableNativeFeedback.Ripple('#000', false)}
         >
-          <View style={[styles.container, { backgroundColor: color }]}>
+          <View style={[styles.container, { backgroundColor: color }, style]}>
             <Text style={styles.label}>{label}</Text>
           </View>
         </TouchableNativeFeedback>
