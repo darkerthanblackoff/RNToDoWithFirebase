78338a20115cba58539807eb2c276f16e8938b1e
Mihail Lemeza <darkerthanblackoff@gmail.com>
2020-05-08 20:57:50 +0300

    Add routes constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,15 @@
+enum MAIN_FLOW {
+  TASKS = 'My Task',
+  MENU = 'Menu',
+  QUICK = 'Quick',
+  PROFILE = 'Profile',
+}
+
+export const ROUTES = {
+  SIGN_IN: 'Sign In',
+  SIGN_UP: 'Sign Up',
+  ONBOARDING: 'Onboard',
+  FORGOT_PASS: 'Forgot',
+  RESET_PASS: 'Reset',
+  MAIN_FLOW: MAIN_FLOW,
+};
