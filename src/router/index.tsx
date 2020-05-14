6882dd7f3f1b2e321a7db8ba372147f531b9f5e0
Mihail Lemeza <darkerthanblackoff@gmail.com>
2020-05-14 16:02:18 +0300

    Add AUTH_FLOW

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,35 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import * as VIEWS from '../views';
 import { ROUTES } from '../constants';
 import { BottomTabBar } from '../components';
+import { STORAGE_KEYS } from '../constants';
 
 const TabNav = createBottomTabNavigator();
+const AuthStack = createStackNavigator();
 
-const Router = () => (
-  <NavigationContainer>
-    <TabNav.Navigator tabBar={BottomTabBar}>
-      <TabNav.Screen
-        name={ROUTES.MAIN_FLOW.TASKS}
-        component={VIEWS.MainFlowScreens.TasksScreen}
+const getAuthStack = (isFirstLaunch: string) => (
+  <AuthStack.Navigator
+    initialRouteName={ROUTES.AUTH_FLOW.INITIAL_SCREEN}
+    headerMode="none"
+  >
+    {isFirstLaunch === 'true' ? (
+      <AuthStack.Screen
+        name={ROUTES.AUTH_FLOW.INITIAL_SCREEN}
+        component={VIEWS.OnboardingScreen}
       />
-      <TabNav.Screen
-        name={ROUTES.MAIN_FLOW.MENU}
-        component={VIEWS.MainFlowScreens.MenuScreen}
+    ) : (
+      <AuthStack.Screen
+        name={ROUTES.AUTH_FLOW.INITIAL_SCREEN}
+        component={VIEWS.AuthScreen}
       />
-      <TabNav.Screen
-        name={ROUTES.MAIN_FLOW.QUICK}
-        component={VIEWS.MainFlowScreens.QuickScreen}
-      />
-      <TabNav.Screen
-        name={ROUTES.MAIN_FLOW.PROFILE}
-        component={VIEWS.MainFlowScreens.ProfileScreen}
-      />
-    </TabNav.Navigator>
-  </NavigationContainer>
+    )}
+    <AuthStack.Screen
+      name={ROUTES.AUTH_FLOW.SIGN_IN}
+      component={VIEWS.SignInScreen}
+    />
+    <AuthStack.Screen
+      name={ROUTES.AUTH_FLOW.SIGN_UP}
+      component={VIEWS.SignUpScreen}
+    />
+    <AuthStack.Screen
+      name={ROUTES.AUTH_FLOW.FORGOT_PASS}
+      component={VIEWS.ForgotPasswordScreen}
+    />
+    <AuthStack.Screen
+      name={ROUTES.AUTH_FLOW.RESET_PASS}
+      component={VIEWS.ResetPasswordScreen}
+    />
+  </AuthStack.Navigator>
 );
 
+const getTabNav = () => (
+  <TabNav.Navigator tabBar={BottomTabBar}>
+    <TabNav.Screen
+      name={ROUTES.MAIN_FLOW.TASKS}
+      component={VIEWS.MainFlowScreens.TasksScreen}
+    />
+    <TabNav.Screen
+      name={ROUTES.MAIN_FLOW.MENU}
+      component={VIEWS.MainFlowScreens.MenuScreen}
+    />
+    <TabNav.Screen
+      name={ROUTES.MAIN_FLOW.QUICK}
+      component={VIEWS.MainFlowScreens.QuickScreen}
+    />
+    <TabNav.Screen
+      name={ROUTES.MAIN_FLOW.PROFILE}
+      component={VIEWS.MainFlowScreens.ProfileScreen}
+    />
+  </TabNav.Navigator>
+);
+
+const Router = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = useState('true');
+  const [isAuthentificated, setAuthentificated] = useState(false);
+  useEffect(() => {
+    AsyncStorage.getItem(STORAGE_KEYS.FIRST_LAUNCH)
+      .then((value) => {
+        setIsFirstLaunch(`${value === null ? true : false}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [isFirstLaunch]);
+  useEffect(() => {}, [isAuthentificated]);
+
+  return (
+    <NavigationContainer>
+      {!isAuthentificated ? getAuthStack(isFirstLaunch) : getTabNav()}
+    </NavigationContainer>
+  );
+};
 export default Router;
