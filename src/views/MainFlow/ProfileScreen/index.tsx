94dd8978374c3f5f8cc2be96300c90b8d7f80d9e
Mihail Lemeza <darkerthanblackoff@gmail.com>
2020-05-14 21:51:38 +0300

    Add Log Out button

--- src/views/MainFlow/ProfileScreen/index.tsx
+++ src/views/MainFlow/ProfileScreen/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Button } from '../../../components';
+import Firebase from '../../../config/firebase';
 
-const ProfileScreenView = () => <View />;
+const ProfileScreenView = () => (
+  <View>
+    <Button
+      label="Log Out"
+      onPress={() => {
+        Firebase.auth().signOut();
+      }}
+    />
+  </View>
+);
 
 export default ProfileScreenView;
